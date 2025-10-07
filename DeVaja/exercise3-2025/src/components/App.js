40e269dd8ba018a41f9baf2d7da07bb5f87d6e98
tickBit <marjukosk@gmail.com>
2025-10-07 08:55:51 +0300

    Add reset password

--- DeVaja/exercise3-2025/src/components/App.js
+++ DeVaja/exercise3-2025/src/components/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from "../contexts/AuthContext"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Login from "./Login"
 import Profile from "./Profile"
+import ForgotPassword from "./ForgotPassword"
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/profile" element={<Profile />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
             </Routes>
           </AuthProvider>
         </Router>
